a1e11371cb52f84829a2ab06efbc86204ee563f3
WillowInTheWind <123593045+WillowInTheWind@users.noreply.github.com>
2023-01-27 10:37:51 -0600

    initial commit

---
+++ python_review.py
@@ -0,0 +1,8 @@
+def sum_of_numbers(number_1, number_2):
+	total = number_1+number_2
+	print("Your sum is " + str(total))
+	return total 	
+
+
+sum_of_numbers(float(input("Give a number ")), float(input("Give another number ")))
+
