b8281e98f10884c2fb0a62f9226287199487a0b9
WillowInTheWind <123593045+WillowInTheWind@users.noreply.github.com>
2023-03-10 10:49:33 -0600

    activity 4+5

--- Css-Activities/linker.py
+++ Css-Activities/linker.py
@@ -2,6 +2,6 @@ Wants_Fonts = True
 
 while Wants_Fonts:
 	font = input("What font do you want to use? ")
-	print("<link href='https://fonts.googleapis.com/css?family=" + str(font) + "'rel='stylesheet'>")
-	if input("do you want another font(Y/N)? ") == "n":
+	print("<link href='https://fonts.googleapis.com/css?family=" + str(font) + "' rel='stylesheet'>")
+	if input("do you want another font(Y/N)? ") == "N":
 		Wants_Fonts = False
