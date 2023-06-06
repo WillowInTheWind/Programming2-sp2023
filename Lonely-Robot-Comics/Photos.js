b825ae1f7bdcb010d47ee02f631e562f012e1685
WillowInTheWind <123593045+WillowInTheWind@users.noreply.github.com>
2023-06-05 20:04:59 -0500

    Final

---
+++ Lonely-Robot-Comics/Photos.js
@@ -0,0 +1,14 @@
+let images = [
+    "https://lh3.google.com/pw/AJFCJaXU-KhfgIOQAuSwlgm8cKfwRQta67caoLQFfxJoZvL4rYPWtahMhOXuoY-OxMXZsmGwuzxTa1KOr7R5XneM2nv2OZMzuw=w814-h1220-s-no?authuser=2",
+    "https://lh3.google.com/pw/AJFCJaVNpBS4mXi-idPw-AScps0M7wo38PbigscyKIPlXUGSYz3iO1IF_s8BEmL7yJnrXMKLwYiJZRmowa8459QNiCwZygchow=w814-h1220-s-no?authuser=2"
+];
+
+//Api boilerplate key
+//Get the Json file from (Album? Whole Account?)
+//move each class in Json into list? Grab directly from Json
+
+for (let i=0;i<2;i++) {
+    id = 'HomepageIMG'+String(i);
+    const imgr = document.getElementById(id);
+    imgr.src = images[Math.floor(Math.random()*2)];
+};
